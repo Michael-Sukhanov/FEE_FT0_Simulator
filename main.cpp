e41500e84ce31f3256330528b760f44f1220be50
Mikhail Sukhanov <sukhanov.mikle@yandex.ru>
2021-01-05 15:54:38 +0100

    It is possible now to abandon write in log if it necessary

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QCoreApplication::setApplicationName("FEE T0 Emulator");
-    QCoreApplication::setApplicationVersion("0.1");
+    QCoreApplication::setApplicationVersion("0.2");
     MainWindow w;
     w.show();
 //    Log log(true, "IPbusServer");
