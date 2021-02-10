c12a765605c158b4f20ac5bb3e9487199a0d446f
Nikitavoz <Nikitavoz@yandex.ru>
2021-02-10 21:29:39 +0300

    from Nikitavoz: logfile checkbox opens and closes the file

--- log.cpp
+++ log.cpp
@@ -5,21 +5,27 @@ Log::Log(const bool createlogfile, const QString caller_name, const QString logF
     if(createlogfile){
         if(!QDir("Logs").exists()) QDir().mkdir("Logs");
         logfile.setFileName(logFileName);
-        if (!logfile.open(QFile::WriteOnly | QIODevice::Append | QFile::Text))
-            this->sendMessage("Can't open log file: " + logFileName, console);
-        else
-            out.setDevice(&logfile);
     }
 }
+
+void Log::openLogFile() {
+    if (!logfile.open(QFile::WriteOnly | QIODevice::Append | QFile::Text))
+        this->sendMessage("Can't open log file: " + logfile.fileName(), console);
+    else
+        out.setDevice(&logfile);
+}
+
+void Log::closeLogFile() { logfile.close(); }
 
 void Log::sendMessage(QString message, OutputDevice device){
     switch (device) {
     case console: {
-        printf(qPrintable("[" + QTime::currentTime().toString("hh:mm:ss") + "] " + this->callername + " >> " + message + "\n"));
+        //printf(qPrintable("[" + QTime::currentTime().toString("hh:mm:ss") + "] " + this->callername + " >> " + message + "\n"));
+        printf("[%s] %s >> %s\n", qPrintable(QTime::currentTime().toString("hh:mm:ss")), qPrintable(QTime::currentTime().toString("hh:mm:ss")), qPrintable(message));
         break;}
     case file:    {
         if(message.contains("IPbus")) message = "\n" + message;
-        if(saveToFile) out    << message << endl;
+        if(saveToFile) out    << message << Qt::endl;
         emit LogMessage(message);
         break;}
     case both:    {
